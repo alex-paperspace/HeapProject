5752f6fbcab271aa410bfcaf991828635fcfc684
Alex Malynovsky <alex@paperspace.com>
2021-11-19 18:02:03 -0500

    mempoolfound

--- HeapManager/src/benchmarks/Improved.cpp
+++ HeapManager/src/benchmarks/Improved.cpp
@@ -18,38 +18,40 @@ namespace benchmark {
 
 		using namespace pool;
 
-		std::vector<MemoryPool<Point>*> memPools;
-		MemoryPool<Point>* init_mem_pool = new MemoryPool<Point>();
-		memPools.push_back(init_mem_pool);
-
-		int mempoolsize = 4096;
 		
-		try {
-			init_mem_pool->setPoolSize(mempoolsize);
-		}
-		catch (const std::invalid_argument& e) {
-			delete init_mem_pool;
-			std::cerr << "Invalid arg: " << e.what() << "\nExiting..." << std::endl;
-			exit(EXIT_FAILURE);
-		}
-		
-		MemoryPool<Point>* currentPool = init_mem_pool;
 
-		for (size_t i = 0; i < m_pgeneric_vector_size; ++i) {
-			Point* current = currentPool->allocate();
-			if (current == nullptr) {
-				//std::cout << "Current Pool full. Creating new Pool..." << std::endl;
-				currentPool = new MemoryPool<Point>();
-				currentPool->setPoolSize(mempoolsize);
-				current = currentPool->allocate();
-				memPools.push_back(currentPool);
-			}
-			m_pgeneric_vector[i] = new (current) Point(double(i), (double)i);
-		}
+		//std::vector<MemoryPool<Point>*> memPools;
+		//MemoryPool<Point>* init_mem_pool = new MemoryPool<Point>();
+		//memPools.push_back(init_mem_pool);
 
-		for (auto i = memPools.begin(); i != memPools.end(); ++i) {
-			delete *i;
-		}
+		//int mempoolsize = 4096;
+		//
+		//try {
+		//	init_mem_pool->setPoolSize(mempoolsize);
+		//}
+		//catch (const std::invalid_argument& e) {
+		//	delete init_mem_pool;
+		//	std::cerr << "Invalid arg: " << e.what() << "\nExiting..." << std::endl;
+		//	exit(EXIT_FAILURE);
+		//}
+		//
+		//MemoryPool<Point>* currentPool = init_mem_pool;
+
+		//for (size_t i = 0; i < m_pgeneric_vector_size; ++i) {
+		//	Point* current = currentPool->allocate();
+		//	if (current == nullptr) {
+		//		//std::cout << "Current Pool full. Creating new Pool..." << std::endl;
+		//		currentPool = new MemoryPool<Point>();
+		//		currentPool->setPoolSize(mempoolsize);
+		//		current = currentPool->allocate();
+		//		memPools.push_back(currentPool);
+		//	}
+		//	m_pgeneric_vector[i] = new (current) Point(double(i), (double)i);
+		//}
+
+		//for (auto i = memPools.begin(); i != memPools.end(); ++i) {
+		//	delete *i;
+		//}
 	}
 
 }
--- HeapManager/src/memorypool.h
+++ HeapManager/src/memorypool.h
@@ -1,63 +1,15 @@
 #pragma once
 
-#include <map>
-
 namespace pool {
 
-	template<class T>
-	class MemoryPool {
-	private:
-		char* m_buffer;
-		unsigned int m_itemSize;
-		unsigned int m_items;
-		std::map<T*, bool> m_used;
-	public:
-
-		MemoryPool() :
-			m_itemSize(sizeof(T))
-		{
-
-		}
-		
-		void setPoolSize(unsigned int poolsize) 
-		{
-			if (poolsize < 2) {
-				throw std::invalid_argument("Specified memory pool size too low for type size.");
-			}
-			poolsize = poolsize - (poolsize % m_itemSize);
-			m_items = poolsize / m_itemSize;
-			m_buffer = new char[poolsize];
-			T* j = reinterpret_cast<T*>(m_buffer);
-			for (unsigned int i = 0; i < m_items; ++i) {
-				m_used[j] = false;
-				j++;
-			}
-			//std::cout << "Memory pool created with poolsize: " << poolsize << std::endl;
-		} 
-
-		~MemoryPool() {
-			delete[] m_buffer;
-		}
-
-		T* allocate() {
-			for (auto it = m_used.begin(); it != m_used.end(); ++it) {
-				if (it->second == false) {
-					it->second = true;
-					return it->first;
-				}
-			}
-			return nullptr;
-		}
-
-		void deallocate(T*& dptr) {
-			m_used[dptr] = false;
-			dptr = nullptr;
-		}
-
-	private:
-		MemoryPool(const MemoryPool&) = delete;
-		MemoryPool& operator=(const MemoryPool&) = delete;
+	union Chunk {
+		char* next;
+		char buffer[8];
 	};
 
+	class Pool {
+
+	};
+	
 }
 

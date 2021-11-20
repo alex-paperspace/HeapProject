8f0ee46140f8e8731b0d1ad3d80bfc6db9c54ece
Alex Malynovsky <alex@paperspace.com>
2021-11-19 19:28:26 -0500

    dummy

--- HeapManager/src/memorypool.h
+++ HeapManager/src/memorypool.h
@@ -1,15 +1,29 @@
 #pragma once
 
+#include <variant>
+#include <list>
+
 namespace pool {
 
-	union Chunk {
-		char* next;
-		char buffer[8];
-	};
-
-	class Pool {
-
+	template<class T>
+	class Chunk {
+		std::variant<char*, T> w;
 	};
 	
+
+	template<class T>
+	class Pool {
+		std::list<T> m_list;
+	public:
+		Pool(size_t count);
+	};
+
+	template<class T>
+	inline Pool<T>::Pool(size_t count) :
+		m_list(count)
+	{
+
+	}
+
 }
 
